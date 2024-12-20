cdfc34bc9b4400adb5c7ee6b0628b0207c53b054
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-21 06:44:45 +0700

    chore(lib): complete server data retrieval

---
+++ src/lib/posts/index.ts
@@ -0,0 +1,42 @@
+import {api} from ".."
+import {IPostForm} from "../../services/types/posts/post"
+
+export const createPost = async (body: IPostForm) => {
+   const res = await api.post("/posts", body)
+   return res.data
+}
+
+export const getAllpost = async (userId: number) => {
+   const res = await api.get(`/posts/${userId}`)
+
+   return res.data
+}
+
+export const getUserById = async (userId: number) => {
+   const response = await api.get(`/users/${userId}`)
+   return response.data
+}
+
+export const toggleReadStatus = async (id: number) => {
+   const res = await api.patch(`/posts/${id}/toggle-read`)
+
+   return res.data
+}
+
+export const getPostById = async (id: number) => {
+   const res = await api.get(`/posts/${id}`)
+
+   return res.data
+}
+
+export const updatePost = async (id: number, body: IPostForm) => {
+   const res = await api.put(`/posts/${id}`, body)
+
+   return res.data
+}
+
+export const removePost = async (id: number) => {
+   const res = await api.delete(`/posts/${id}`)
+
+   return res.data
+}
