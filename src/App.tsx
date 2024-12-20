8d0a3ac55a540686c11501423b223782ddc7ce28
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-21 06:55:20 +0700

    fix(app): fix bug function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import {useEffect, useState} from "react"
+import "./App.css"
+import {api, setAuthToken} from "./lib"
+import useStore from "./stores/hook"
+import Home from "./components/home/index"
+import Login from "./components/auth/login/index"
+import Register from "./components/auth/register/index"
+import Index from "./components/posts/index"
+import Navigation from "./assets/common/navigation"
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [showNavbar, setShowNavbar] = useState(true)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   const {setUser} = useStore()
+
+   async function checkAuth() {
+      const token = localStorage.getItem("token")
+      if (!token) {
+         return
+      }
+
+      try {
+         const res = await api.get("/auth/me", {
+            headers: {
+               Authorization: `Bearer ${token}`,
+            },
+         })
+
+         setUser(res.data)
+         setAuthToken(token)
+      } catch (error) {
+         throw error
+      }
+   }
+
+   useEffect(() => {
+      checkAuth()
+   }, [])
+
+   return (
+      <Router>
+         {showNavbar && <Navigation />}
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+               path="/auth/login"
+               element={<Login setShowNavbar={setShowNavbar} />}
+            />
+            <Route
+               path="/auth/register"
+               element={<Register setShowNavbar={setShowNavbar} />}
+            />
+            <Route path="/todo" element={<Index />} />
+         </Routes>
+      </Router>
+   )
 }
 
 export default App
