e4555c8e829a0c0bd2f6512b859797994cc429a5
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-21 10:06:06 +0700

    chore(lib): fix url deploy

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL || "http://localhost:2000",
+   baseURL:
+      import.meta.env.VITE_API_URL || "https://knowledge-test-api.vercel.app",
 })
 
 export const setAuthToken = (token: string) => {
