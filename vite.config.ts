8e92f211ea49f9965d845e46e8904cae96e3af04
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-22 01:58:43 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+import legacy from "@vitejs/plugin-legacy"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [
+      react(),
+      legacy({
+         targets: ["> 1%", "last 2 versions", "iOS >= 10", "not dead"],
+      }),
+   ],
 })
